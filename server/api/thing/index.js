0025568ca4d8c8b5812a8243d5403be48ef03eb9
Isaac <alberto@albertovillalobos.me>
2016-07-06 22:18:52 -0700

    authentication across endpoints

--- server/api/thing/index.js
+++ server/api/thing/index.js
@@ -8,10 +8,10 @@ var controller = require('./thing.controller');
 var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/:id', auth.hasRole('admin'), controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
